8557e8dd0333392ca6b817424e32ee69075d114c
fainla <ainlapoiss@hot.ee>
2014-10-15 11:45:07 +0300

    final 15.10
    Lõpp jäi poolikuks

--- form_processing.php
+++ form_processing.php
@@ -5,6 +5,42 @@
    
   </head>
   <body>
-<p>Tere Maailm!</p>
+
+<pre>
+<?php
+//print_r($_POST);
+$username = $_POST["username"];
+$password = $_POST["password"];
+//echo "Tere {$username}!" . "Teie parool on {$password}.";
+//print_r($_POST);
+?>
+</pre>
+
+<?php
+
+  /*if (isset($_POST["username"])) {
+    $username = $_POST["username"];
+
+  } else {
+    $username = "puudub";
+  }*/
+  $username = isset($_POST["username"]) ? $_POST["username"]) : "Puudub";
+
+  /*if (isset($_POST["password"])) {
+    $password = $_POST["password"];
+     
+  } else {
+   
+    $password = "puudub";
+  }*/
+  $password = isset($_POST["password"]) ? $_POST["password"]) : "Puudub";
+
+  if ($username == "puudub" || $username == "" || $password == "puudub" ||  $password == "") {
+//echo "Kasutajanimi või parool on puudu.";
+  	echo "Kasutajanimi või parool on puudu.";
+  } else {
+echo "Tere {$username}!" . "Teie parool on {$password}.";
+  }
+?>
   </body>
 </html>
